c096e2485338a82901bf4a0b6fa833ccdac36d7b
Javkhaa <bayarjavkhlanbatsukh.com>
2025-02-12 16:06:12 +0800

    2 commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GetStarted from "./feature/getStarted/pages/getStarted";
+import Login from "./feature/login/pages/login";
+import { useNavigation } from "@react-navigation/native";
 
-export default function App() {
+function HomeScreen() {
+  const navigate = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button title="getStarted " onPress={() => navigate.push("GetStarted")} />
+      <Button title="click nme" onPress={() => navigate.push("Login")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screens: {
+    Home: HomeScreen,
+    GetStarted: GetStarted,
+    Login: Login,
   },
 });
+
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
